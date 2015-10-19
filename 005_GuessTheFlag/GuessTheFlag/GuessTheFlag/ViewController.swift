aba59aaa57e73eabcd469c4776182681d9359a6c
Javan.Chen <fu6gjp4@gmail.com>
2015-10-19 11:13:19 +0800

    005 add

---
+++ 005_GuessTheFlag/GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by javan.chen on 2015/10/19.
+//  Copyright © 2015年 Javan chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
