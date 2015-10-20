1b0f108db3c5a6a70e580c895e5ed47286f8ef8e
Javan.Chen <fu6gjp4@gmail.com>
2015-10-20 13:36:49 +0800

    007 可彈性增加網址 easy browser完成

--- 007_EasyBrowser/EasyBrowser/EasyBrowser/ViewController.swift
+++ 007_EasyBrowser/EasyBrowser/EasyBrowser/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
     var progressView: UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com"]
     
     override func loadView() {
         webView = WKWebView()
@@ -36,16 +37,16 @@ class ViewController: UIViewController, WKNavigationDelegate {
         toolbarItems = [progressButton, spacer, refresh]
         navigationController?.toolbarHidden = false
         //open app first http
-        let url = NSURL(string: "https://www.hackingwithswift.com")!
+        let url = NSURL(string: "https://" + websites[0])!
         webView.loadRequest(NSURLRequest(URL: url))
         webView.allowsBackForwardNavigationGestures = true
     }
     
     func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .ActionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .Default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .Default, handler: openPage))
+        }
         presentViewController(ac, animated: true, completion: nil)
     }
     
@@ -56,6 +57,19 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.URL
+        if let host = url!.host {
+            for website in websites {
+                if host.rangeOfString(website) != nil {
+                    decisionHandler(.Allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.Cancel)
     }
 
     override func didReceiveMemoryWarning() {
