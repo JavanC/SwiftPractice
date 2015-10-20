fa584599a7fb27387c20b11e987479dd04cc67b1
Javan.Chen <fu6gjp4@gmail.com>
2015-10-20 10:45:56 +0800

    007 增加選擇開啟網頁alert 及 title 顯示目前網頁title

--- 007_EasyBrowser/EasyBrowser/EasyBrowser/ViewController.swift
+++ 007_EasyBrowser/EasyBrowser/EasyBrowser/ViewController.swift
@@ -22,9 +22,27 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: "openTapped")
         let url = NSURL(string: "https://www.hackingwithswift.com")!
         webView.loadRequest(NSURLRequest(URL: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    func openPage(action: UIAlertAction!) {
+        let url = NSURL(string: "https://" + action.title!)!
+        webView.loadRequest(NSURLRequest(URL: url))
+    }
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        title = webView.title
     }
 
     override func didReceiveMemoryWarning() {
