b632312e4614a24149c4f85e125318c69fb18a3d
Javan.Chen <fu6gjp4@gmail.com>
2015-10-21 14:24:30 +0800

    008 初始化 輸入單字檔 顯示於第一欄 table view

--- 008_WordScramble/WordScramble/WordScramble/DetailViewController.swift
+++ 008_WordScramble/WordScramble/WordScramble/DetailViewController.swift
@@ -1,44 +1,44 @@
 //
 //  DetailViewController.swift
-//  WordScramble
+//  Project5
 //
-//  Created by javan.chen on 2015/10/21.
-//  Copyright © 2015年 Javan chen. All rights reserved.
+//  Created by Hudzilla on 13/09/2015.
+//  Copyright © 2015 Paul Hudson. All rights reserved.
 //
 
 import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+	@IBOutlet weak var detailDescriptionLabel: UILabel!
 
 
-    var detailItem: AnyObject? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
-    }
+	var detailItem: AnyObject? {
+		didSet {
+		    // Update the view.
+		    self.configureView()
+		}
+	}
 
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
+	func configureView() {
+		// Update the user interface for the detail item.
+		if let detail = self.detailItem {
+		    if let label = self.detailDescriptionLabel {
+		        label.text = detail.description
+		    }
+		}
+	}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		self.configureView()
+	}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
 
 }
