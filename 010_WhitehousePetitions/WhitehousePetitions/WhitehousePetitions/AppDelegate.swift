83c8abcf0d7755a4e2c9d971721d28bea66812ba
JavanC <fu6gjp4@gmail.com>
2015-10-25 22:22:58 +0800

    010 add top rated , show error function 010 done!

--- 010_WhitehousePetitions/WhitehousePetitions/WhitehousePetitions/AppDelegate.swift
+++ 010_WhitehousePetitions/WhitehousePetitions/WhitehousePetitions/AppDelegate.swift
@@ -20,6 +20,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
         splitViewController.delegate = self
+        // add other masterviewcontroller
+        let tabBarController = splitViewController.viewControllers[0] as! UITabBarController
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewControllerWithIdentifier("NavController") as! UINavigationController
+        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .TopRated, tag: 1)
+        
+        tabBarController.viewControllers?.append(vc)
+        // end other masterviewcontroller
         return true
     }
 
--- 010_WhitehousePetitions/WhitehousePetitions/WhitehousePetitions/MasterViewController.swift
+++ 010_WhitehousePetitions/WhitehousePetitions/WhitehousePetitions/MasterViewController.swift
@@ -17,15 +17,27 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
         
         if let url = NSURL(string: urlString) {
             if let data = try? NSData(contentsOfURL: url, options: []) {
                 let json = JSON(data: data)
                 
                 if json["metadata"]["responseInfo"]["status"].intValue == 200 {
-                    parseJSON(json)                }
+                    parseJSON(json)
+                } else {
+                    showError()
+                }
+            } else {
+                showError()
             }
+        } else {
+            showError()
         }
     }
 
@@ -82,6 +94,13 @@ class MasterViewController: UITableViewController {
             objects.append(obj)
         }
         tableView.reloadData()
+    }
+    
+    //showError
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
     }
 }
 
