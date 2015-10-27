47ae3b8e0882c6b9836afd86fb0b2572abbaf3a3
Javan.Chen <fu6gjp4@gmail.com>
2015-10-27 17:16:33 +0800

    013 add Name to face setting up done

---
+++ 013_NamesToFaces /NamesToFaces/NamesToFaces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by javan.chen on 2015/10/27.
+//  Copyright © 2015年 Javan chen. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
---
+++ 013_NamesToFaces /NamesToFaces/NamesToFaces/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by javan.chen on 2015/10/27.
+//  Copyright © 2015年 Javan chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
