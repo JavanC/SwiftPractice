c273385d445b934055cf0011883148568e7fa5ec
Javan.Chen <fu6gjp4@gmail.com>
2015-10-28 14:59:02 +0800

    014 add bounciness ball

--- 014_Pachinko/Pachinko/Pachinko/GameScene.swift
+++ 014_Pachinko/Pachinko/Pachinko/GameScene.swift
@@ -18,14 +18,21 @@ class GameScene: SKScene {
         
         physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
     }
-    
+
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.locationInNode(self)
-            let box = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 64, height: 64))
-            box.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 64, height: 64))
-            box.position = location
-            addChild(box)
+            /// box physics body
+            //let box = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 64, height: 64))
+            //box.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 64, height: 64))
+            //box.position = location
+            //addChild(box)
+            /// ball physics body
+            let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+            ball.physicsBody!.restitution = 0.4
+            ball.position = location
+            addChild(ball)
         }
     }
    
