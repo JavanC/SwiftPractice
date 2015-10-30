09d6947927b013479880c1c851184b40105bfd47
Javan.Chen <fu6gjp4@gmail.com>
2015-10-30 16:17:23 +0800

    014 add destroyball effects : fireParticles 014 don

--- 014_Pachinko/Pachinko/Pachinko/GameScene.swift
+++ 014_Pachinko/Pachinko/Pachinko/GameScene.swift
@@ -161,6 +161,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func destroyBall(ball: SKNode) {
+        if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
+            fireParticles.position = ball.position
+            addChild(fireParticles)
+        }
+        
         ball.removeFromParent()
     }
 
