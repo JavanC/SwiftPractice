e888ecad65197ce70baaf22bfc3c380b90827534
Javan.Chen <fu6gjp4@gmail.com>
2015-11-03 17:27:41 +0800

    016 add save to photos library function 016 done!

--- 016_CoreImage/016_CoreImage/ViewController.swift
+++ 016_CoreImage/016_CoreImage/ViewController.swift
@@ -85,7 +85,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func save(sender: UIButton) {
+        UIImageWriteToSavedPhotosAlbum(imageView.image!, self, "image:didFinishSavingWithError:contextInfo:", nil)
     }
+    func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafePointer<Void>) {
+        if error == nil {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+        } else {
+            let ac = UIAlertController(title: "Save error", message: error?.localizedDescription, preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+        }
+    }
+    
     @IBAction func intensityChanged(sender: UISlider) {
         applyProcessing()
     }
