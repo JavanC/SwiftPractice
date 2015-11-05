07e48a2a92cf21587d9b511252949c4d17d326c8
Javan.Chen <fu6gjp4@gmail.com>
2015-11-05 10:20:20 +0800

    019 UIText, finalize and done function

--- 019_SafariJavaScript/SafariJavaScript/Extension/ActionViewController.swift
+++ 019_SafariJavaScript/SafariJavaScript/Extension/ActionViewController.swift
@@ -11,17 +11,25 @@ import MobileCoreServices
 
 class ActionViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
+    var pageTitle = ""
+    var pageURL = ""
+    
+    @IBOutlet weak var script: UITextView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad(navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "done"))
         
         if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
                 itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil) { [unowned self] (dict,error) in
                     let itemDictionary = dict as! NSDictionary
                     let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
-                    print(javaScriptValues)
+                    self.pageTitle = javaScriptValues["title"] as! String
+                    self.pageURL = javaScriptValues["URL"] as! String
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.title = self.pageTitle
+                    }
                 }
             }
         }
@@ -34,9 +42,12 @@ class ActionViewController: UIViewController {
     }
     
     @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequestReturningItems(self.extensionContext!.inputItems, completionHandler: nil)
+        let item = NSExtensionItem()
+        let webDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: ["customJavaScript": script.text]]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        
+        extensionContext!.completeRequestReturningItems([item], completionHandler: nil)
     }
     
 }
