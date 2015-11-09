f95f1a1a574045c37540c1da195161359e868858
Javan.Chen <fu6gjp4@gmail.com>
2015-11-09 14:01:18 +0800

    020 Ninja add

---
+++ 020_Ninja/Ninja/Ninja/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Ninja
+//
+//  Created by javan.chen on 2015/11/9.
+//  Copyright (c) 2015年 Javan chen. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+ 
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
