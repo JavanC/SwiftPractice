73aae912a83e0a4059ae7ccebfb42bf8b275db32
Javan.Chen <fu6gjp4@gmail.com>
2015-11-12 10:08:50 +0800

    021 iAd&Debugging add

---
+++ 021_iAd&Debugging/iAd&Debugging/iAd&Debugging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iAd&Debugging
+//
+//  Created by javan.chen on 2015/11/12.
+//  Copyright © 2015年 Javan chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
