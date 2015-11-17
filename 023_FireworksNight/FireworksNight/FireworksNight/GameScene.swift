f70db17ad1b27e18769e9f63e5cdee7d8dc12249
Javan.Chen <fu6gjp4@gmail.com>
2015-11-17 14:33:17 +0800

    023 Fireworks Night add

---
+++ 023_FireworksNight/FireworksNight/FireworksNight/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  FireworksNight
+//
+//  Created by javan.chen on 2015/11/17.
+//  Copyright (c) 2015年 Javan chen. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
