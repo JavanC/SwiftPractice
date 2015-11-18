f7030db9a111661d2aea4fdfb9bd0d56cb192c03
Javan.Chen <fu6gjp4@gmail.com>
2015-11-18 16:32:36 +0800

    024 add register and schedule Local function

--- 024_LocalNotification/LocalNotification/LocalNotification/ViewController.swift
+++ 024_LocalNotification/LocalNotification/LocalNotification/ViewController.swift
@@ -14,7 +14,22 @@ class ViewController: UIViewController {
         UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
     }
     @IBAction func scheduleLocal(sender: AnyObject) {
+        guard let settings = UIApplication.sharedApplication().currentUserNotificationSettings() else { return }
         
+        if settings.types == .None {
+            let ac = UIAlertController(title: "Can't schedule", message: "Either we don't have permission to schedule notifications, or we haven't asked yet.", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+            return
+        }
+        
+        let notification = UILocalNotification()
+        notification.fireDate = NSDate(timeIntervalSinceNow: 5)
+        notification.alertBody = "Hey you! Yeah you! Swipe to unlock!"
+        notification.alertAction = "be awesome!"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.userInfo = ["CustomField1": "w00t"]
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
     }
 
     override func viewDidLoad() {
