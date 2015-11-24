f961b71f9ebf6d898bb46733f3cc8545f5f5ff21
Javan.Chen <fu6gjp4@gmail.com>
2015-11-24 18:02:59 +0800

    027 extension Int function and UIColor

--- 027_SwiftExtension/SwiftExtension.playground/Contents.swift
+++ 027_SwiftExtension/SwiftExtension.playground/Contents.swift
@@ -2,4 +2,30 @@
 
 import UIKit
 
-var str = "Hello, playground"
+extension Int {
+    mutating func plusOne() {
+        ++self
+    }
+    
+    static func random(min min: Int, max: Int) -> Int {
+        if max < min { return min }
+        return Int(arc4random_uniform(UInt32((max - min) + 1))) + min
+    }
+}
+
+extension UIColor {
+    static func chartreuseColor() -> UIColor {
+        return UIColor(red: 0.875, green: 1, blue: 0, alpha: 1)
+    }
+}
+
+var myInt = 0
+
+myInt.plusOne()
+
+myInt
+
+var otherInt = 10
+otherInt.plusOne()
+
+Int.random(min: 1, max: 10)
