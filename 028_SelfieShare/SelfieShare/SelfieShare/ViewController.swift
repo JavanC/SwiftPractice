66a106ee726f830492131c3c8098daa146c810c0
Javan.Chen <fu6gjp4@gmail.com>
2015-11-26 11:29:40 +0800

    028 add session did receive data function

--- 028_SelfieShare/SelfieShare/SelfieShare/ViewController.swift
+++ 028_SelfieShare/SelfieShare/SelfieShare/ViewController.swift
@@ -98,5 +98,41 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         presentViewController(mcBrowser, animated: true, completion: nil)
     }
     
+    // MC Function
+    func session(session: MCSession, didReceiveStream stream: NSInputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    func session(session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, withProgress progress: NSProgress) {
+        
+    }
+    func session(session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, atURL localURL: NSURL, withError error: NSError?) {
+        
+    }
+    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
+        switch state {
+        case MCSessionState.Connected:
+            print("Connected: \(peerID.displayName)")
+            
+        case MCSessionState.Connecting:
+            print("Connecting: \(peerID.displayName)")
+            
+        case MCSessionState.NotConnected:
+            print("Not Connected: \(peerID.displayName)")
+        }
+    }
+    
+    func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
+        if let image = UIImage(data: data) {
+            dispatch_async(dispatch_get_main_queue()) { [unowned self] in
+                self.images.insert(image, atIndex: 0)
+                self.collectionView.reloadData()
+            }
+        }
+    }
 }
-
