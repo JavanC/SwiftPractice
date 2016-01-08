7e343d3d3d5355e0b28437633d0d7161a7815558
Javan.Chen <fu6gjp4@gmail.com>
2016-01-08 11:40:27 +0800

    031 touch id add

---
+++ 031_TouchID/TouchID/TouchID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchID
+//
+//  Created by javan.chen on 2016/1/8.
+//  Copyright © 2016年 Javan chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
