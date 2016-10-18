352a3667298268a4d46f0d19dbd96de4118c6c6b
Javan.Chen <fu6gjp4@gmail.com>
2016-10-18 15:12:34 +0800

    finish Air Aber watchOS app

--- 034_WatchOS_AirAber/AirAber/Watch Extension/ScheduleInterfaceController.swift
+++ 034_WatchOS_AirAber/AirAber/Watch Extension/ScheduleInterfaceController.swift
@@ -12,31 +12,46 @@ import Foundation
 
 class ScheduleInterfaceController: WKInterfaceController {
 
-    @IBOutlet var flightsTable: WKInterfaceTable!
-    var flights = Flight.allFlights()
+  @IBOutlet var flightsTable: WKInterfaceTable!
+  var flights = Flight.allFlights()
+  var selectedIndex = 0
   
-    override func awake(withContext context: Any?) {
-      super.awake(withContext: context)
-      flightsTable.setNumberOfRows(flights.count, withRowType: "FlightRow")
-      for index in 0..<flightsTable.numberOfRows {
-        guard let controller = flightsTable.rowController(at: index) as? FlightRowController else { continue }
-        controller.flight = flights[index]
-      }
+  override func awake(withContext context: Any?) {
+    super.awake(withContext: context)
+    flightsTable.setNumberOfRows(flights.count, withRowType: "FlightRow")
+    for index in 0..<flightsTable.numberOfRows {
+      guard let controller = flightsTable.rowController(at: index) as? FlightRowController else { continue }
+      controller.flight = flights[index]
     }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+  }
+  
+  override func didAppear() {
+    super.didAppear()
+    guard flights[selectedIndex].checkedIn,
+      let controller = flightsTable.rowController(at: selectedIndex) as? FlightRowController else {
+        return
     }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    
+    animate(withDuration: 0.35) {
+      controller.updateForCheckIn()
     }
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
   
   override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
     let flight = flights[rowIndex]
-    presentController(withName: "Flight", context: flight)
+    let controllers = ["Flight", "CheckIn"]
+    
+    selectedIndex = rowIndex
+    presentController(withNames: controllers, contexts: [flight, flight])
   }
-
 }
