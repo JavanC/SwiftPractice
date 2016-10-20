e5c3d406ffb5757f61a590a61616b28f155c0622
Javan.Chen <fu6gjp4@gmail.com>
2016-10-20 18:01:35 +0800

    try use sceneKit

---
+++ 035_GeometryFighter/GeometryFighter/GeometryFighter/GameViewController.swift
@@ -0,0 +1,65 @@
+//
+//  GameViewController.swift
+//  GeometryFighter
+//
+//  Created by Javan.Chen on 2016/10/20.
+//  Copyright © 2016年 Javan.Chen. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class GameViewController: UIViewController {
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    var cameraNode: SCNNode!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupScene()
+        setupCamera()
+        spawnShape()
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func setupView() {
+        scnView = self.view as! SCNView
+        
+        scnView.showsStatistics = true
+        scnView.allowsCameraControl = true
+        scnView.autoenablesDefaultLighting = true
+    }
+    
+    func setupScene() {
+        scnScene = SCNScene()
+        scnView.scene = scnScene
+        scnScene.background.contents = "GeometryFighter.scnassets/Textures/Background_Diffuse.png"
+    }
+    
+    func setupCamera() {
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+        scnScene.rootNode.addChildNode(cameraNode)
+    }
+    
+    func spawnShape() {
+        var geometry: SCNGeometry
+        
+        switch ShapeType.random() {
+        default:
+            geometry = SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0)
+        }
+        
+        let geometryNode = SCNNode(geometry: geometry)
+        scnScene.rootNode.addChildNode(geometryNode)
+    }
+}
