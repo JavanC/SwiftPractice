0dc5c7de287d7fec4a055b5fbc2c07aa43a5d3c2
Javan.Chen <fu6gjp4@gmail.com>
2016-10-21 10:51:44 +0800

    add spawn loop and clean node

--- 035_GeometryFighter/GeometryFighter/GeometryFighter/GameViewController.swift
+++ 035_GeometryFighter/GeometryFighter/GeometryFighter/GameViewController.swift
@@ -13,13 +13,14 @@ class GameViewController: UIViewController {
     var scnView: SCNView!
     var scnScene: SCNScene!
     var cameraNode: SCNNode!
+    var spawnTime: TimeInterval = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupScene()
         setupCamera()
-        spawnShape()
+//        spawnShape()
     }
     
     override var shouldAutorotate: Bool {
@@ -36,6 +37,10 @@ class GameViewController: UIViewController {
         scnView.showsStatistics = true
         scnView.allowsCameraControl = true
         scnView.autoenablesDefaultLighting = true
+        
+        scnView.delegate = self
+        
+        scnView.isPlaying = true
     }
     
     func setupScene() {
@@ -47,7 +52,7 @@ class GameViewController: UIViewController {
     func setupCamera() {
         cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+        cameraNode.position = SCNVector3(x: 0, y: 5, z: 10)
         scnScene.rootNode.addChildNode(cameraNode)
     }
     
@@ -55,11 +60,57 @@ class GameViewController: UIViewController {
         var geometry: SCNGeometry
         
         switch ShapeType.random() {
-        default:
-            geometry = SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0)
+        case .box:
+            geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        case .sphere:
+            geometry = SCNSphere(radius: 0.5)
+        case .pyramid:
+            geometry = SCNPyramid(width: 1.0, height: 1.0, length: 1.0)
+        case .torus:
+            geometry = SCNTorus(ringRadius: 0.5, pipeRadius: 0.25)
+        case .capsule:
+            geometry = SCNCapsule(capRadius: 0.3, height: 2.5)
+        case .cylinder:
+            geometry = SCNCylinder(radius: 0.3, height: 2.5)
+        case .cone:
+            geometry = SCNCone(topRadius: 0.25, bottomRadius: 0.5, height: 1.0)
+        case .tube:
+            geometry = SCNTube(innerRadius: 0.25, outerRadius: 0.5, height: 1.0)
         }
         
+        geometry.materials.first?.diffuse.contents = UIColor.random()
+        
         let geometryNode = SCNNode(geometry: geometry)
+        
+        geometryNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        
+        let randomX = Float.random(min: -2, max: 2)
+        let randomY = Float.random(min: 10, max: 18)
+        
+        let force = SCNVector3Make(randomX, randomY, 0)
+        
+        let position = SCNVector3Make(0.05, 0.05, 0.05)
+        
+        geometryNode.physicsBody?.applyForce(force, at: position, asImpulse: true)
+    
         scnScene.rootNode.addChildNode(geometryNode)
+    }
+    
+    func cleanScene() {
+        for node in scnScene.rootNode.childNodes {
+            if node.presentation.position.y < -2 {
+                node.removeFromParentNode()
+            }
+        }
+    }
+}
+
+extension GameViewController: SCNSceneRendererDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if time > spawnTime {
+            spawnShape()
+            spawnTime = time + TimeInterval(Float.random(min: 0.2, max: 1.5))
+        }
+        cleanScene()
     }
 }
