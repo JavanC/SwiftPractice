224e6464ba744ddcfd9a18f2ebf0d6d07bc25f93
Javan.Chen <fu6gjp4@gmail.com>
2016-12-30 10:47:33 +0800

    test cocoapod test

---
+++ 037_CocoaPodsTest/CocoaPodsTest/CocoaPodsTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CocoaPodsTest
+//
+//  Created by Javan.Chen on 2016/12/30.
+//  Copyright © 2016年 Javan.Chen. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 037_CocoaPodsTest/CocoaPodsTest/Pods/Target Support Files/Pods-CocoaPodsTest/Pods-CocoaPodsTest-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_CocoaPodsTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CocoaPodsTestVersionString[];
+
